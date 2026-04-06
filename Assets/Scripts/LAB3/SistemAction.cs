ab20e090a60921a1942e81477cad6b66b0eca86b
ChemsLGDesu <jamesgonelozano@gmail.com>
2026-04-05 22:05:27 -0500

    Implementacion final de mover, atacar y defender

--- Assets/Scripts/LAB3/SistemAction.cs
+++ Assets/Scripts/LAB3/SistemAction.cs
@@ -1,12 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
 public class SistemAction : MonoBehaviour
 {
     private NodeOfActionPlayer inicio;
-    private NodeOfActionPlayer final;
-    
-    public void  AgregarAccion(PlayerAction action)
+    private NodeOfActionPlayer fin;
+
+    public void InputMover() => AgregarAccion(PlayerAction.Mover);
+    public void InputAtacar() => AgregarAccion(PlayerAction.Atacar);
+    public void InputDefender() => AgregarAccion(PlayerAction.Defender);
+    public void BotonPlay() => EjecutarTurno();
+
+    public void  AgregarAccion(PlayerAction accion)
     {
-        NodeOfActionPlayer nuevoModo = new NodeOfActionPlayer(action);
+        NodeOfActionPlayer nuevoNodo = new NodeOfActionPlayer(accion);
+        
+        if (inicio == null)
+        {
+            inicio = nuevoNodo;
+            fin = nuevoNodo;
+        }
+        else
+        {
+            fin.next = nuevoNodo;
+            fin = nuevoNodo;
+        }
+        Debug.Log("Acción añadida a la cola: " + accion);
+    }
+
+    public void EjecutarTurno()
+    {
+        if (inicio == null)
+        {
+            Debug.LogWarning("No hay acciones en la lista");
+            return;
+        }
+        StartCoroutine(RutinaEjecucion());
+    }
+
+    private IEnumerator RutinaEjecucion()
+    {
+        NodeOfActionPlayer actual = inicio;
+        Debug.Log("Iniciando Repetición Automática del Turno");
+
+        while (actual != null)
+        {
+            Debug.Log("Ejecutando acción: " + actual.accion);
+            actual = actual.next;
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        Debug.Log("Fin de la secuencia");
+
+        inicio = null;
+        fin = null;
     }
 }
